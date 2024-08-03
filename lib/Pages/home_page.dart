333e036c05fcb6b756b1603d1c35323dcd22d559
Sandu Robert Cristian <91720736+8TILMATE@users.noreply.github.com>
2024-08-03 15:27:43 +0300

    chatting now works

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:satelite_chatting_app/Pages/chat_page.dart';
 import 'package:satelite_chatting_app/services/auth/auth_service.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +11,7 @@ class HomePage extends StatefulWidget {
 } 
 class _HomePageState extends State<HomePage>{
 
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   void signOut(){
     final authService = Provider.of<AuthService>(context,listen: false);
     authService.signOut();
@@ -21,6 +25,46 @@ class _HomePageState extends State<HomePage>{
           IconButton(onPressed: signOut, icon: const Icon(Icons.logout)),
         ],
         ),
+        body: _buildUserList(),
     );
+  }
+  Widget _buildUserList(){
+     return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('users').snapshots(),
+        builder:(context, snapshot){
+          if(snapshot.hasError){
+            return const Text('error');
+          }
+          if(snapshot.connectionState== ConnectionState.waiting){
+            return const Text("Loading..");
+          }
+          return ListView(
+            children: snapshot.data!.docs
+              .map<Widget>((doc)=> _buildUserListItem(doc))
+              .toList(),
+          );
+        },
+      );
+  }
+  Widget _buildUserListItem(DocumentSnapshot document){
+    Map<String,dynamic> data = document.data()! as Map<String,dynamic>;
+
+    if(_auth.currentUser!.email!=data['email']){
+      return ListTile(
+          title: Text(data['email']),
+          onTap: (){
+            // go to chat
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ChatPage(
+                  receiverEmail: data['email'],
+                  receiverUID: data['uid'],
+                ),
+                )
+              );
+          },
+      );
+    } else return Container();
   }
 }
